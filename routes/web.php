343c3222156701c8e77d851976422a183853f97a
aliyyaps <aliyyaputris23@gmail.com>
2022-04-19 22:28:20 +0700

    Praktikum 1 - Add Route Resource Articles

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 
 /*
@@ -22,3 +23,6 @@ Route::get('search', [MahasiswaController::class, 'search'])->name('search');
 
 // Latihan JS 9 (Langkah 10)
 Route::get('mahasiswa/nilai/{nim}', [MahasiswaController::class, 'nilai'])->name('nilai');
+
+// Praktikum 1 JS 10 (Langkah 6)
+Route::resource('articles', ArticleController::class);
